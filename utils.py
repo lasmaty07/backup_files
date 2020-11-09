30d7b2f9a0c39ff7983d949dedc5c5b9509be1fc
Matias Brigante <matias.brigante@gmail.com>
2020-11-09 19:26:56 -0300

    initial commit

---
+++ utils.py
@@ -0,0 +1,27 @@
+import os, math
+from hurry.filesize import size, iec
+
+def get_size(start_path = '.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+
+    return total_size
+
+
+def convert_size(size_bytes):
+   if size_bytes == 0:
+       return "0B"
+   size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+   i = int(math.floor(math.log(size_bytes, 1024)))
+   p = math.pow(1024, i)
+   s = round(size_bytes / p, 2)
+   return "%s %s" % (s, size_name[i])
+
+
+def size_converted(path):
+	return(convert_size(get_size(path)))
