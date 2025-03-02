0f05a636dcf2a067dd13b8430c56edbf7cbdf877
Matt Brigante <matias.brigante@bigbox.com.ar>
2025-03-02 15:36:27 -0300

    fix rsync for sudo crontab

--- backup.zsh
+++ backup.zsh
@@ -3,21 +3,24 @@
 source ./secrets.zsh
 
 echo 
-echo $(date)
+echo "=======================backup $(date)======================="
+echo 
 echo "destination: $dest_backupdir"
 echo "source: $source_backupdir"
 echo
 
-# Run rsync
-rsync -rltvz --exclude-from=exclude-list.txt -e "ssh -i \"/home/matt/.ssh/id_ed25519\"" $source_backupdir $dest_backupdir --delete
+
+rsync -rltvz --exclude-from=exclude-list.txt -e "sudo -u $user_impersonate ssh" $source_backupdir $dest_backupdir --delete
 
 EXIT_CODE=$?
 
-# Send exit status to Telegram
+
 MESSAGE="Backup from $HOSTNAME ($OS_NAME $OS_VERSION) completed.\nRsync Exit Code: $EXIT_CODE"
 curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
      -H "Content-Type: application/json" \
      -d "{\"chat_id\": \"$CHAT_ID\", \"text\": \"$MESSAGE\"}"
 
-# Exit with rsync exit code
+
+echo 
+echo "=======================end of backup======================="
 exit $EXIT_CODE
